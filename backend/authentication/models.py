b536af57e73e48a883fa09fa3fef059478fdcaaf
muratom <rm070201wr@gmail.com>
2022-03-15 17:45:49 +0300

    Fix a non-cross-platform bug

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -123,7 +123,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
         token = jwt.encode({
             'id': self.pk,
-            'exp': int(dt.strftime('%s'))
+            'exp': int(dt.strftime('%S'))
         }, settings.SECRET_KEY, algorithm='HS256')
 
         return token
