bd0f3ee03a629978535585607f95d2a625ab2745
daniilmikulik <mikulik086420@gmail.com>
2022-03-20 23:00:03 +0300

    image upload handler added

--- frontend/src/components/ViewPanel.js
+++ frontend/src/components/ViewPanel.js
@@ -7,6 +7,7 @@ import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 import { Suspense } from "react";
 import * as THREE from "three";
 import { DDSLoader } from "three-stdlib";
+import axios from "axios";
 
 THREE.DefaultLoadingManager.addHandler(/\.dds$/i, new DDSLoader());
 
@@ -16,6 +17,41 @@ const Scene = () => {
 };
 
 class ViewPanel extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            photos: []
+        }
+    }
+
+    handleUpload = (event) => {
+        const data = new FormData();
+        data.append('file', event.target.files[0]);
+        let requestUrl = 'http://localhost:8000/upload';
+        const config = {
+            headers: { 'content-type': 'multipart/form-data' }
+        }
+
+        console.log(event.target.files);
+
+        //axios.post(requestUrl, data, config)
+        //    .then((response) => {
+        //        this.setState({ photos: [response.data, ...this.state.photos] });
+        //    })
+        //    .catch((error) => {
+        //        console.log(error);
+        //    });
+    }
+
+    /*
+    <Grid data-testid={'view-panel'} container spacing={2}>
+                {this.state.photos.map(photo => (
+                    <Grid item xl={4} xs={12}>
+                        <img src={`http://localhost:3000/${photo.filename}`} alt={'user photo'} />
+                    </Grid>
+                ))}
+    </Grid>
+    */
     render(){
         return (
             <Grid data-testid={'view-panel'} container spacing={2}>
@@ -27,6 +63,7 @@ class ViewPanel extends React.Component {
                         id="raised-button-file"
                         multiple
                         type="file"
+                        onChange={this.handleUpload}
                     />
                     <label htmlFor="raised-button-file">
                         <Button variant="raised" component="span" sx={{ marginLeft: '30%', marginTop: '10%' }}>
