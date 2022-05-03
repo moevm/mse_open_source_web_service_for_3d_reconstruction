893f441eb350fea172e304be28071e51c728ddfc
muratom <rm070201wr@gmail.com>
2022-05-03 19:22:51 +0500

    Added production support for backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -133,12 +133,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-MEDIA_ROOT = BASE_DIR / 'media'
 # URL that handles the media served from MEDIA_ROOT, used for managing stored files.
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
