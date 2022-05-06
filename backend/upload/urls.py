a909b128164f3a263772681beef9ee394d04168a
R0st0k <2002rostok@gmail.com>
2022-05-06 14:44:28 +0300

    Add names to links

--- backend/upload/urls.py
+++ backend/upload/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'authentication'
 urlpatterns = [
     path('', views.UploadView.as_view(), name='upload_dataset'),
-    path('status/', views.StatusView.as_view()),
-    path('download/', views.DownloadView.as_view()),
+    path('status/', views.StatusView.as_view(), name='get_status'),
+    path('download/', views.DownloadView.as_view(), name='download_project'),
 ]
